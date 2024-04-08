54ea03b50cd27332e2b29baeb2c7f296ffe93aa2
Alex Abdugafarov <fswork90@gmail.com>
2024-04-09 02:01:18 +0500

    Playing with Tauri menu

--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -4,9 +4,10 @@
 use std::fs;
 use std::path::Path;
 use std::str::FromStr;
+use std::sync::{Arc, Mutex};
 
 use lazy_static::lazy_static;
-use tauri::{AppHandle, Manager, Runtime};
+use tauri::AppHandle;
 use tauri::menu::{Menu, MenuId, MenuItem, PredefinedMenuItem, Submenu};
 use tauri_plugin_dialog::{DialogExt, MessageDialogKind};
 
@@ -44,48 +45,95 @@ fn report_error_string(handle: AppHandle, error: String) {
 }
 
 lazy_static! {
+    static ref MENU_ID_DATABASE: MenuId = MenuId::from_str("database").unwrap();
+    static ref MENU_ID_EDIT: MenuId = MenuId::from_str("edit").unwrap();
+
     static ref MENU_ID_OPEN: MenuId = MenuId::from_str("open").unwrap();
+    static ref MENU_ID_USERS: MenuId = MenuId::from_str("users").unwrap();
+    static ref MENU_ID_MERGE_DATASETS: MenuId = MenuId::from_str("merge_datasets").unwrap();
+    static ref MENU_ID_COMPARE_DATASETS: MenuId = MenuId::from_str("compare_datasets").unwrap();
 }
 
 pub fn start() {
-    fn make_menu<R, M>(handle: &M) -> tauri::Result<Menu<R>>
-        where R: Runtime, M: Manager<R>
-    {
-        // First menu will be a main dropdown menu on macOS
-        let file_menu = Submenu::with_items(
-            handle, "File", true,
-            &[
-                &MenuItem::with_id(handle, MENU_ID_OPEN.clone(), "Open", true, None::<&str>)?,
-                &PredefinedMenuItem::separator(handle)?,
-                &PredefinedMenuItem::quit(handle, None)?,
-            ])?;
-
-        Menu::with_items(handle, &[&file_menu])
-    }
+    let pre_db_sep_id = Arc::new(Mutex::<Option<MenuId>>::from(None));
+    let pre_db_sep_id_clone = pre_db_sep_id.clone();
 
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
-        .setup(|app| {
+        .setup(move |app| {
             let handle = app.handle();
-            app.set_menu(make_menu(handle)?)?;
-            app.on_menu_event(move |app, event| match event.id() {
-                x if x == &*MENU_ID_OPEN => {
-                    app.dialog()
-                        .file()
-                        .add_filter("Markdown", &["md"])
-                        .pick_file(|path_buf| match path_buf {
-                            Some(p) => { println!("Selected {p:?}") }
-                            _ => {}
-                        });
+
+            app.set_menu({
+                let pre_db_sep = PredefinedMenuItem::separator(handle)?;
+                let post_db_sep = PredefinedMenuItem::separator(handle)?;
+                let mut pre_db_sep_id = pre_db_sep_id_clone.lock().unwrap();
+                *pre_db_sep_id = Some(pre_db_sep.id().clone());
+
+                // First menu will be a main dropdown menu on macOS
+                let file_menu = Submenu::with_id_and_items(
+                    handle, MENU_ID_DATABASE.clone(), "Database", true,
+                    &[
+                        &MenuItem::with_id(handle, MENU_ID_OPEN.clone(), "Open [NYI]", true, None::<&str>)?,
+                        &pre_db_sep,
+                        &post_db_sep,
+                        &PredefinedMenuItem::quit(handle, None)?,
+                    ])?;
+
+                let edit_menu = Submenu::with_id_and_items(
+                    handle, MENU_ID_EDIT.clone(), "Edit", true,
+                    &[
+                        &MenuItem::with_id(handle, MENU_ID_USERS.clone(), "Users [NYI]", true, None::<&str>)?,
+                        &MenuItem::with_id(handle, MENU_ID_MERGE_DATASETS.clone(), "Merge Datasets [NYI]", true, None::<&str>)?,
+                        &MenuItem::with_id(handle, MENU_ID_COMPARE_DATASETS.clone(), "Compare Datasets [NYI]", true, None::<&str>)?,
+                    ])?;
+
+                Menu::with_items(handle, &[&file_menu, &edit_menu])?
+            })?;
+
+            app.on_menu_event(move |handle, event| {
+                let pre_db_sep_id = pre_db_sep_id.clone();
+                match event.id() {
+                    x if x == &*MENU_ID_OPEN => {
+                        let handle = handle.clone();
+                        handle
+                            .dialog()
+                            .file()
+                            .add_filter("Own format", &["sqlite"])
+                            .pick_file(move |path_buf| match path_buf {
+                                Some(p) => {
+                                    let menu = handle.menu().unwrap();
+                                    let pre_db_sep_id = pre_db_sep_id.lock()
+                                        .expect("lock separator id");
+                                    let pre_db_sep_id = pre_db_sep_id.as_ref()
+                                        .expect("get separator id value");
+                                    let main_menu = &menu.items()
+                                        .expect("get menu items")[0];
+                                    let main_menu = main_menu.as_submenu_unchecked();
+                                    let items = main_menu.items()
+                                        .expect("get main menu items");
+                                    let pre_db_sep_idx = items
+                                        .iter()
+                                        .position(|item| item.id() == pre_db_sep_id)
+                                        .expect("find separator position");
+
+                                    let new_item =
+                                        MenuItem::with_id(&handle, format!("db_"), p.name.unwrap(), true, None::<&str>)
+                                            .expect("create menu item");
+                                    main_menu.insert_items(&[&new_item], pre_db_sep_idx + 1)
+                                        .expect("add db menu item");
+                                }
+                                _ => { /* No file picked */ }
+                            });
+                    }
+                    _ => {}
                 }
-                _ => {}
             });
 
             Ok(())
         })
-        .menu(|handle| {
-            make_menu(handle)
-        })
+        // .menu(|handle| {
+        //     make_menu(handle)
+        // })
         .invoke_handler(tauri::generate_handler![open_popup, report_error_string, read_file_base64])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
